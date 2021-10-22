87e8a58db66631dde1530669cee1e12991e698e6
Stefan Kuhn <semptic@kuhn.cloud>
2021-10-22 12:23:44 +0200

    Move stack machine into Fiber

--- src/main/scala/sio/Sio.scala
+++ src/main/scala/sio/Sio.scala
@@ -7,36 +7,23 @@ import scala.concurrent.ExecutionContext
 trait Fiber[+A]:
   def join: Sio[A]
 
-class FiberImpl[A](sio: Sio[A]) extends Fiber[A]:
-  val result = AtomicReference[Option[A]](None)
-  ExecutionContext.global.execute(() => result.lazySet(Some(sio.runUnsafeSync)))
+private class FiberImpl[A](startSio: Sio[A]) extends Fiber[A]:
+  private val result = AtomicReference[Option[A]](None)
+  ExecutionContext.global.execute(run _)
 
-  override def join: Sio[A] =
-    while (result.get().isEmpty) do Thread.sleep(100)
-
-    Sio.succeedNow(result.get().get)
-
-sealed trait Sio[+A]:
-  final def flatMap[B](cont: A => Sio[B]): Sio[B] = Sio.FlatMap(this, cont)
-  final def map[B](cont: A => B): Sio[B]          = this.flatMap(a => Sio.succeed(cont(a)))
-  final def zip[B](that: Sio[B]): Sio[(A, B)]     = this.flatMap(a => that.flatMap(b => Sio.succeed((a, b))))
-
-  final def fork: Sio[Fiber[A]] = Sio.Fork(this)
-
-  final def runUnsafeSync: A =
+  private def run() =
     type Erased = Sio[Any]
     type Cont   = Any => Erased
 
     def erase[A](sio: Sio[A]): Erased = sio
 
-    var result: Any = null
-    val stack       = Stack.empty[Cont]
-    var currentSio  = erase(this)
-    var loop        = true
+    val stack      = Stack.empty[Cont]
+    var currentSio = erase(startSio)
+    var loop       = true
 
-    def complete[A](value: A) =
+    def complete(value: Any) =
       if (stack.isEmpty)
-        result = value
+        result.lazySet(Some(value.asInstanceOf[A]))
         loop = false
       else
         val cont = stack.pop()
@@ -58,17 +45,32 @@ sealed trait Sio[+A]:
         case Sio.Fork(sio) =>
           currentSio = erase(Sio.succeedNow(new FiberImpl(sio)))
 
-    result.asInstanceOf[A]
+  override def join: Sio[A] =
+    while (result.get().isEmpty) do Thread.sleep(100)
+
+    Sio.succeedNow(result.get().get)
+
+sealed trait Sio[+A]:
+  final def flatMap[B](cont: A => Sio[B]): Sio[B] = Sio.FlatMap(this, cont)
+  final def map[B](cont: A => B): Sio[B]          = this.flatMap(a => Sio.succeed(cont(a)))
+  final def zip[B](that: Sio[B]): Sio[(A, B)]     = this.flatMap(a => that.flatMap(b => Sio.succeed((a, b))))
+
+  final def fork: Sio[Fiber[A]] = Sio.Fork(this)
+
+  final def runUnsafeSync: A = runUnsafe.join.asInstanceOf[Sio.Succeed[A]].value
+
+  final def runUnsafe: Fiber[A] =
+    new FiberImpl[A](this)
 
 object Sio:
   def succeedNow[A](value: A): Sio[A]             = Succeed(value)
   def succeed[A](thunk: => A): Sio[A]             = Effect(() => thunk)
   def async[A](f: (Sio[A] => Any) => Any): Sio[A] = Async(f)
 
-  private case class Succeed[A](value: A)                extends Sio[A]
-  private case class Effect[A](thunk: () => A)           extends Sio[A]
-  private case class Async[A](f: (Sio[A] => Any) => Any) extends Sio[A]
+  case class Succeed[A](value: A)                extends Sio[A]
+  case class Effect[A](thunk: () => A)           extends Sio[A]
+  case class Async[A](f: (Sio[A] => Any) => Any) extends Sio[A]
 
-  private case class FlatMap[A, B](sio: Sio[A], cont: A => Sio[B]) extends Sio[B]
+  case class FlatMap[A, B](sio: Sio[A], cont: A => Sio[B]) extends Sio[B]
 
-  private case class Fork[A](sio: Sio[A]) extends Sio[Fiber[A]]
+  case class Fork[A](sio: Sio[A]) extends Sio[Fiber[A]]
