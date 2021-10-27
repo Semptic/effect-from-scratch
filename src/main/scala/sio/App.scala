76e0a62166663bb473029633e505cc7959de26a9
Stefan Kuhn <semptic@kuhn.cloud>
2021-10-27 20:08:47 +0200

    Convert App to Sio

--- src/main/scala/sio/App.scala
+++ src/main/scala/sio/App.scala
@@ -1,7 +1,11 @@
 package sio
 
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+def printEffect(msg: String): Sio[Nothing, Unit] = Sio.succeed(println(msg))
 
 def msg = "I was compiled by Scala 3. :)"
+
+def program =
+  Sio.succeed("Hello world!").flatMap(printEffect).zipRight(Sio.succeed(msg)).flatMap(printEffect)
+
+@main def hello: Unit =
+  program.runUnsafeSync.fold(println, println)
