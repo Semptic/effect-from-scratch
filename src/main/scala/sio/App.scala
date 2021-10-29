36659fa08428e09b6a2905f1f603a7a67ccdcc48
Stefan Kuhn <semptic@kuhn.cloud>
2021-10-29 22:20:14 +0200

    Add more stuff to app

--- src/main/scala/sio/App.scala
+++ src/main/scala/sio/App.scala
@@ -1,11 +1,27 @@
-package sio
+package not.sio
+
+import sio.{Result, Sio}
+
+import scala.util.Random
 
 def printEffect(msg: String): Sio[Nothing, Unit] = Sio.succeed(println(msg))
 
 def msg = "I was compiled by Scala 3. :)"
 
-def program =
-  Sio.succeed("Hello world!").flatMap(printEffect).zipRight(Sio.succeed(msg)).flatMap(printEffect)
+def program = Sio
+  .succeed("Hello world!")
+  .flatMap(printEffect)
+  .zipRight(Sio.succeed(msg))
+  .flatMap(printEffect)
+  .zipRight(Sio.async[String, Int] { complete =>
+    Random.nextInt(3) match
+      case 0 => complete(Sio.succeed(12))
+      case 1 => complete(Sio.fail("Bad luck"))
+      case 2 => complete(Sio.die(Exception("Really bad luck")))
+  })
 
 @main def hello: Unit =
-  println(program.runUnsafeSync) // Todo: Create api for Result to map/fold over it
+  program.runUnsafeSync match
+    case Result.Success(s)   => println(s"Success: $s")
+    case Result.Error(e)     => println(s"Error: $e")
+    case Result.Exception(t) => println(s"Exception $t")
