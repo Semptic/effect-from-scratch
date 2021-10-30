aed24583760cf8cadf8dd44050024787a5988633
Stefan Kuhn <semptic@kuhn.cloud>
2021-10-30 09:31:58 +0200

    Reformat tests

--- src/test/scala/sio/SioSpec.scala
+++ src/test/scala/sio/SioSpec.scala
@@ -45,9 +45,8 @@ class SioSpec extends AnyFlatSpec with Matchers:
     messages.poll() shouldBe "Hello, world!"
 
   it should "flatMap" in new Fixture:
-    val sio = Sio.succeed(7)
-
-    val mapped = sio
+    Sio
+      .succeed(7)
       .flatMap(a =>
         Sio.async[Nothing, Int] { complete =>
           Future {
@@ -57,31 +56,25 @@ class SioSpec extends AnyFlatSpec with Matchers:
         }
       )
       .flatMap(a => Sio.succeed(a - 7))
-
-    mapped.runUnsafeSync shouldBe Result.Success(42)
+      .runUnsafeSync shouldBe Result.Success(42)
 
   it should "map" in new Fixture:
-    val sio = Sio.succeed(7)
-
-    val mapped = sio.map(a => a * 7).map(a => a - 7)
-
-    mapped.runUnsafeSync shouldBe Result.Success(42)
+    Sio
+      .succeed(7)
+      .map(a => a * 7)
+      .map(a => a - 7)
+      .runUnsafeSync shouldBe Result.Success(42)
 
   it should "zip" in new Fixture:
-    val sio1 = Sio.succeed(8)
-    val sio2 = Sio.succeed(9)
-
-    val zipped = sio1 zip sio2
+    val zipped = Sio.succeed(8) zip Sio.succeed(9)
 
     zipped.runUnsafeSync shouldBe Result.Success((8, 9))
 
   it should "zipWith" in new Fixture:
-    val sio1 = Sio.succeed(7)
-    val sio2 = Sio.succeed(6)
-
-    val zipped = sio1.zipWith(sio2)((a, b) => a * b)
-
-    zipped.runUnsafeSync shouldBe Result.Success(42)
+    Sio
+      .succeed(7)
+      .zipWith(Sio.succeed(6))((a, b) => a * b)
+      .runUnsafeSync shouldBe Result.Success(42)
 
   it should "zipLeft" in new Fixture:
     (Sio.succeed(42) <* Sio.succeed(0)).runUnsafeSync shouldBe Result.Success(42)
@@ -90,11 +83,10 @@ class SioSpec extends AnyFlatSpec with Matchers:
     (Sio.succeed(0) *> Sio.succeed(42)).runUnsafeSync shouldBe Result.Success(42)
 
   it should "repeat" in new Fixture:
-    val sio = Sio.succeed(sendMessage("Hello, world!"))
-
-    val repeated = sio.repeat(10)
-
-    repeated.runUnsafeSync shouldBe Result.Success(())
+    Sio
+      .succeed(sendMessage("Hello, world!"))
+      .repeat(10)
+      .runUnsafeSync shouldBe Result.Success(())
 
     messages.size() shouldBe 11
     (0 until 11).foreach { _ =>
@@ -102,11 +94,10 @@ class SioSpec extends AnyFlatSpec with Matchers:
     }
 
   it should "repeat 0 times" in new Fixture:
-    val sio = Sio.succeed(sendMessage("Hello, world!"))
-
-    val repeated = sio.repeat(0)
-
-    repeated.runUnsafeSync shouldBe Result.Success(())
+    Sio
+      .succeed(sendMessage("Hello, world!"))
+      .repeat(0)
+      .runUnsafeSync shouldBe Result.Success(())
 
     messages.size() shouldBe 1
     messages.poll() shouldBe "Hello, world!"
@@ -114,11 +105,10 @@ class SioSpec extends AnyFlatSpec with Matchers:
   it should "be stack-safe" in new Fixture:
     val n    = 10_000_000
     var runs = 0
-    val sio  = Sio.succeed(runs += 1)
-
-    val repeated = sio.repeat(n - 1)
-
-    repeated.runUnsafeSync shouldBe Result.Success(())
+    Sio
+      .succeed(runs += 1)
+      .repeat(n - 1)
+      .runUnsafeSync shouldBe Result.Success(())
 
     runs shouldBe n
 
@@ -199,27 +189,38 @@ class SioSpec extends AnyFlatSpec with Matchers:
     threadIdC shouldBe threadId
 
   it should "fold" in new Fixture:
-    Sio.fail("Noooooooo").fold(_ => Sio.succeed(42), _ => Sio.succeed(0)).runUnsafeSync shouldBe Result.Success(42)
-    Sio.succeed("Yeeeesss").fold(_ => Sio.succeed(0), _ => Sio.succeed(42)).runUnsafeSync shouldBe Result.Success(42)
+    Sio
+      .fail("Noooooooo")
+      .fold(_ => Sio.succeed(42), _ => Sio.succeed(0))
+      .runUnsafeSync shouldBe Result.Success(42)
+    Sio
+      .succeed("Yeeeesss")
+      .fold(_ => Sio.succeed(0), _ => Sio.succeed(42))
+      .runUnsafeSync shouldBe Result.Success(42)
 
-    Sio.fail("Noooooooo").fold(_ => Sio.fail(42), _ => Sio.succeed(0)).runUnsafeSync shouldBe Result.Error(42)
-    Sio.succeed("Yeeeesss").fold(_ => Sio.succeed(0), _ => Sio.fail(42)).runUnsafeSync shouldBe Result.Error(42)
+    Sio
+      .fail("Noooooooo")
+      .fold(_ => Sio.fail(42), _ => Sio.succeed(0))
+      .runUnsafeSync shouldBe Result.Error(42)
+    Sio
+      .succeed("Yeeeesss")
+      .fold(_ => Sio.succeed(0), _ => Sio.fail(42))
+      .runUnsafeSync shouldBe Result.Error(42)
 
   it should "not run flatMap in case of failure" in new Fixture:
-    val program = Sio
+    Sio
       .fail("Noooooooo")
       .map { _ =>
         sendMessage("Map")
         42
       }
       .zip(Sio.succeed(42))
-
-    program.runUnsafeSync shouldBe Result.Error("Noooooooo")
+      .runUnsafeSync shouldBe Result.Error("Noooooooo")
 
     messages.size() shouldBe 0
 
   it should "catch errors" in new Fixture:
-    val program = Sio
+    Sio
       .fail("Noooooooo")
       .map { _ =>
         sendMessage("Map")
@@ -230,28 +231,27 @@ class SioSpec extends AnyFlatSpec with Matchers:
         Sio.succeed(())
       }
       .zipRight(Sio.succeed(42))
-
-    program.runUnsafeSync shouldBe Result.Success(42)
+      .runUnsafeSync shouldBe Result.Success(42)
 
     messages.size() shouldBe 1
     messages.poll() shouldBe "Noooooooo"
 
   it should "handle exceptions" in new Fixture:
-    val program = Sio.succeed(throw Throwable("Noooooo"))
+    val result = Sio
+      .succeed(throw Throwable("Noooooo"))
+      .runUnsafeSync
 
-    val result = program.runUnsafeSync
     result shouldBe a[Result.Exception]
     result.asInstanceOf[Result.Exception].throwable.getMessage shouldBe "Noooooo"
 
   it should "recover from exceptions" in new Fixture:
-    val program = Sio
+    Sio
       .succeed(5 / 0)
       .catchException { t =>
         sendMessage(t.getMessage)
         Sio.succeed(0)
       }
-
-    program.runUnsafeSync shouldBe Result.Success(0)
+      .runUnsafeSync shouldBe Result.Success(0)
 
     messages.size() shouldBe 1
     messages.poll() shouldBe "/ by zero"
