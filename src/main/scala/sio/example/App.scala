280d2a98a1a35f9715cb13bd6ea9a5c9791c3721
Stefan Kuhn <semptic@kuhn.cloud>
2021-11-09 19:59:49 +0100

    Add simple guessing game

--- src/main/scala/sio/example/App.scala
+++ src/main/scala/sio/example/App.scala
@@ -8,7 +8,7 @@ def printEffect(msg: String): Sio[Nothing, Unit] = Sio.succeed(println(msg))
 
 def msg = "I was compiled by Scala 3. :)"
 
-def program = Sio
+def HelloWorldProgram = Sio
   .succeed("Hello world!")
   .flatMap(printEffect)
   .zipRight(Sio.succeed(msg))
@@ -20,8 +20,44 @@ def program = Sio
       case 2 => complete(Sio.die(Exception("Really bad luck")))
   })
 
+def PrintLine(msg: String): Sio[Nothing, Unit]      = Sio.succeed(println(msg))
+def ReadLine(): Sio[Nothing, String]                = Sio.succeed(scala.io.StdIn.readLine())
+def RandomNumber(maxNumber: Int): Sio[Nothing, Int] = Sio.succeed(Random.nextInt(maxNumber))
+
+def GuessNumber(maxNumber: Int): Sio[Nothing, Int] =
+  for
+    _      <- PrintLine(s"Guess a number between 0 (inclusive) and $maxNumber (exclusive):")
+    number <- ReadLine().flatMap(s => Sio.succeed(s.toInt))
+  yield number
+
+def CheckNumber(n: Int, maxNumber: Int): Sio[Nothing, Boolean] =
+  def checkNumber(n: Int, guess: Sio[Nothing, Int]): Sio[Nothing, Boolean] =
+    guess.flatMap { guess =>
+      if guess == n then Sio.succeed(true)
+      else
+        val msg =
+          if guess > n then PrintLine("Too high!")
+          else PrintLine("Too low!")
+
+        msg *> checkNumber(n, GuessNumber(maxNumber))
+    }
+
+  checkNumber(n, GuessNumber(maxNumber))
+
+def GuessingGameProgram(maxNumber: Int) =
+  for
+    randomNumber <- RandomNumber(maxNumber)
+    success      <- CheckNumber(randomNumber, maxNumber)
+  yield success
+
 @main def hello: Unit =
-  program.runUnsafeSync match
+  HelloWorldProgram.runUnsafeSync match
     case Result.Success(s)   => println(s"Success: $s")
+    case Result.Error(e)     => println(s"Error: $e")
+    case Result.Exception(t) => println(s"Exception $t")
+
+@main def guess: Unit =
+  GuessingGameProgram(10).runUnsafeSync match
+    case Result.Success(s)   => println(s"Concrats you found the number")
     case Result.Error(e)     => println(s"Error: $e")
     case Result.Exception(t) => println(s"Exception $t")
